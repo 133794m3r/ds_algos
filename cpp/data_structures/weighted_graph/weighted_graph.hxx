e96fb099b995ccc2ccfbdf34154eec685fbbb942
Macarthur Inbody <133794m3r@gmail.com>
2021-02-06 15:02:37 -0500

    CPP: Renamed private members to end in "_" cpp/data_structures/weighted_graph.hxx: Renamed adj_lists, and vertexes to have trailing _ to help show they are private in code use.

--- cpp/data_structures/weighted_graph/weighted_graph.hxx
+++ cpp/data_structures/weighted_graph/weighted_graph.hxx
@@ -20,51 +20,51 @@ template <typename T, typename U> class WeightedGraph {
 			this->weight = weight_;
 		}
 	};
-	std::unordered_map<T,std::vector<Edge>> adj_lists;
-	ull vertexes;
+	std::unordered_map<T,std::vector<Edge>> adj_lists_;
+	ull vertexes_;
   public:
 	WeightedGraph(){
-		vertexes = 0;
+		vertexes_ = 0;
 	}
 	bool add_vertex(T vertex){
-		if(adj_lists.count(vertex) > 0)
+		if(adj_lists_.count(vertex) > 0)
 			return false;
-		adj_lists[vertex] = std::vector<Edge>();
+		adj_lists_[vertex] = std::vector<Edge>();
 		return true;
 	}
 
 	bool add_edge(T src, T dest, U weight){
-		for(Edge edge: this->adj_lists[src]){
+		for(Edge edge: this->adj_lists_[src]){
 			if(edge.node == dest){
 				return false;
 			}
 		}
-		adj_lists[src].push_back(Edge(dest,weight));
-		adj_lists[dest].push_back(Edge(src,weight));
+		adj_lists_[src].push_back(Edge(dest, weight));
+		adj_lists_[dest].push_back(Edge(src, weight));
 		return true;
 	}
 	void remove_edge(T v1, T v2){
-		for(ull i =0;i<adj_lists[v1].size();i++){
-			if(adj_lists[v1][i].node == v2) {
-				adj_lists[v1].erase(adj_lists[v1].begin() + i);
+		for(ull i =0; i < adj_lists_[v1].size(); i++){
+			if(adj_lists_[v1][i].node == v2) {
+				adj_lists_[v1].erase(adj_lists_[v1].begin() + i);
 				break;
 			}
 		}
-		for(ull i =0;i<adj_lists[v2].size();i++){
-			if(adj_lists[v2][i].node == v1) {
-				adj_lists[v2].erase(adj_lists[v2].begin() + i);
+		for(ull i =0; i < adj_lists_[v2].size(); i++){
+			if(adj_lists_[v2][i].node == v1) {
+				adj_lists_[v2].erase(adj_lists_[v2].begin() + i);
 				break;
 			}
 		}
 	}
 	bool remove_vertex(T v1){
-		if(adj_lists.count(v1) > 0){
-			while(adj_lists[v1].size()){
-				Edge tmp = adj_lists[v1][0];
+		if(adj_lists_.count(v1) > 0){
+			while(adj_lists_[v1].size()){
+				Edge tmp = adj_lists_[v1][0];
 				remove_edge(v1,tmp.node);
 			}
-			adj_lists.erase(v1);
-			vertexes--;
+			adj_lists_.erase(v1);
+			vertexes_--;
 			return true;
 		}
 		else{
@@ -75,14 +75,14 @@ template <typename T, typename U> class WeightedGraph {
 	std::vector<T> dfs(T start){
 		Stack<T> stack(start);
 		std::unordered_map<T,bool> visited{};
-		visited.reserve(vertexes);
+		visited.reserve(vertexes_);
 		visited[start] = true;
 		std::vector<T> result;
-		result.reserve(vertexes);
+		result.reserve(vertexes_);
 		while(stack.size()){
 			T current = stack.pop();
 			result.push_back(current);
-			for(Edge neighbor: adj_lists[current]){
+			for(Edge neighbor: adj_lists_[current]){
 				if(visited.count(neighbor.node) == 0){
 					stack.push(neighbor.node);
 					visited[neighbor.node] = true;
@@ -98,7 +98,7 @@ template <typename T, typename U> class WeightedGraph {
 		std::vector<T> path;
 		T smallest;
 		PriorityQueue<T, ull> nodes;
-		for(auto pair:adj_lists){
+		for(auto pair:adj_lists_){
 			if (pair.first == start){
 				distances[pair.first] = 0;
 				nodes.enqueue(pair.first,0);
@@ -118,7 +118,7 @@ template <typename T, typename U> class WeightedGraph {
 				}
 				break;
 			}
-				for(Edge neighbor: adj_lists[smallest]){
+				for(Edge neighbor: adj_lists_[smallest]){
 					U candidate = distances[smallest] + neighbor.weight;
 					T next_neighbor = neighbor.node;
 					if(candidate < distances[next_neighbor]){
